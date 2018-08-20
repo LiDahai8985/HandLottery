a46d24ce24f0956742b7bd251472ea84a8f69e4d
LiDahai <278118698@qq.com>
2018-08-20 10:02:23 +0800

    Initial Commit

---
+++ HandLottery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HandLottery
+//
+//  Created by jrzl on 2018/8/20.
+//  Copyright © 2018年 jrzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
