ac1a28fd2315103dd469b3a2c084737aad6581ea
LiDahai <278118698@qq.com>
2018-08-20 14:27:55 +0800

    【pod配置添加】

---
+++ Podfile
@@ -0,0 +1,9 @@
+
+platform:ios,'8.0'
+
+target'HandLottery'do
+
+pod 'AFNetworking/Serialization', '~> 3.2.1'
+pod 'Masonry', '~> 1.1.0'
+
+end
---
+++ Pods/Target Support Files/Pods-HandLottery/Pods-HandLottery-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HandLottery : NSObject
+@end
+@implementation PodsDummy_Pods_HandLottery
+@end
